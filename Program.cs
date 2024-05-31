37440dcd7d3aea482060829424c0db182c43cd21
codingbrew <babayagaga07@gmail.com>
2024-05-31 15:34:12 +0300

    veriler tipleri eklendi

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿
+int deger = 2;
+string degisken = null;
+// string class; tanimlanamaz reserved word
+string degisken_veri = null;
+
+byte b = 1;         // 1byte 
+sbyte sb = 1;       // 1byte 
+     
+short s = 2;        // 2byte
+ushort us = 2;      // 2byte
+     
+     
+Int16 i16 = 2;      // 2 byte
+int i = 4;          // 4 byte
+Int32 i32 = 4;      // 4 byte
+Int64 i64 = 8;      // 8 byte
+     
+uint ui = 2;        // 4 byte
+long l = 4;         // 8 byte
+ulong ul = 8;       // 8 byte
+     
+float f = 4;        // 4 byte
+double d = 8;       // 8 byte
+decimal de = 16;    // 16 byte
+
+char ch = '2';      // 2byte 
+string str = "test";   // unlimited
+
+bool b1 = true;
+bool b2 = false;
+
+DateTime dt = DateTime.Now;
+
+Console.WriteLine(deger);
